5258b02c9354f833220ddcdd4a9301e85bf3f8b0
silvia <scocagonzales@gmail.com>
2013-12-04 18:06:27 -0400

    adding comments to the keywords

--- src/algorithm_backtracking.py
+++ src/algorithm_backtracking.py
@@ -12,6 +12,9 @@ class Backtracking(AlgorithmBase):
     def solve(self, sudoku_matrix):
         """
         Return a solved Sudoku Matrix object using the algorithm Backtracking
+
+        keyword arguments 
+        sudoku_matrix -- Sudoku Matrix object without solution
         """
         self.matrix = sudoku_matrix.matrix
         self.solve_in_line(0, 0)
@@ -22,6 +25,10 @@ class Backtracking(AlgorithmBase):
     def solve_in_line(self, row, col):
         """
         Recursive method in order to go over the matrix
+
+        keyword arguments 
+        row -- Number of row of the matrix
+        col -- Number of column of the matrix
         """
         if col >= 9:
             col = 0
@@ -32,9 +39,9 @@ class Backtracking(AlgorithmBase):
         if int(self.matrix[row][col].get_cell_value()) != 0:
             return self.solve_in_line(row, (col + 1))
 
-        for cellValue in range(1, 10):
-            if self.is_valid_value(row, col, cellValue):
-                self.matrix[row][col].set_cell_value(cellValue)
+        for cell_value in range(1, 10):
+            if self.is_valid_value(row, col, cell_value):
+                self.matrix[row][col].set_cell_value(cell_value)
                 if self.solve_in_line(row, (col + 1)):
                     return True
         (self.matrix[row][col]).set_cell_value(0)
@@ -43,6 +50,12 @@ class Backtracking(AlgorithmBase):
     def is_valid_value(self, row, col, cell_value):
         """
         Validate if the value to use is valid, validating the column, the row and the square
+
+        keyword arguments 
+        row -- Number of row of the matrix
+        col -- Number of column of the matrix
+        cell_value -- the candidate value in order to set in the matrix after evaluating in the
+        column, row and square
         """
         col_val = self.is_valid_value_in_column(col, cell_value)
         row_val = self.is_valid_value_in_row(row, cell_value)
@@ -56,6 +69,11 @@ class Backtracking(AlgorithmBase):
     def is_valid_value_in_column(self, col, cell_value):
         """
         Validate if the value to use is valid in the column
+
+        keyword arguments 
+        row -- Number of row of the matrix
+        col -- Number of column of the matrix
+        cell_value -- the candidate value in order to set in the matrix
         """
         for i in range(9):
             if cell_value == self.matrix[i][col].get_cell_value():
@@ -65,6 +83,11 @@ class Backtracking(AlgorithmBase):
     def is_valid_value_in_row(self, row, cell_value):
         """
         Validate if the value to use is valid in the row
+
+        keyword arguments 
+        row -- Number of row of the matrix
+        col -- Number of column of the matrix
+        cell_value -- the candidate value in order to set in the matrix
         """
         for j in range(9):
             if cell_value == self.matrix[row][j].get_cell_value():
@@ -74,6 +97,11 @@ class Backtracking(AlgorithmBase):
     def is_valid_value_in_square(self, row, col, cell_value):
         """
         Validate if the value to use is valid in the square
+
+        keyword arguments 
+        row -- Number of row of the matrix
+        col -- Number of column of the matrix
+        cell_value -- the candidate value in order to set in the matrix
         """
         row_offset = (row / 3) * 3
         col_offset = (col / 3) * 3
