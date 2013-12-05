03c3592795933010eb120ed465a5513ec47af1a4
Carlos Gonzales <carlos.gonzales@jalasoft.com>
2013-12-04 21:16:35 -0400

    Updated the Logger and Singleton meta class according to code review
    Updated the variable and method name in the logger unittest

--- src/logger.py
+++ src/logger.py
@@ -7,34 +7,35 @@ class LoggerManager(object):
 
     __metaclass__ = SingletonMetaClass
 
-    def __init__(self, player_name, logger_file="../sudoku.log"):
+    def __init__(self, log_id_name, log_file_name="../sudoku.log"):
         """Constructor of Logger Manager
 
         Keyword arguments:
-        logger_file -- file name of the log
+        log_file_name -- file name of the log
 
         """
-        file_handler = self.get_file_handler(logger_file)
+        file_handler = self.get_file_handler(log_file_name)
         self.logger = logging.getLogger()
         self.logger.setLevel(logging.DEBUG)
-        formatter = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
+        formatter = logging.Formatter("%(asctime)s %(name)s - %(levelname)s - %(message)s")
         file_handler.setFormatter(formatter)
         self.logger.addHandler(file_handler)
-        self.logger_name = player_name
+        self.logger_name = log_id_name
 
-    def get_file_handler(self, logger_file):
-        """Return the file handler of the log
+    def get_file_handler(self, log_file_name):
+        """The method will convert the log_file_name in a absolute path and Return the file handler
+        of the log_file
 
         Keyword arguments:
-        logger_file -- file name of the log
+        log_file_name -- file name of the log (i.e. sudoku.log)
 
         """
-        self.log_file_name = os.path.abspath(logger_file)
-        handler = logging.FileHandler(self.log_file_name, 'a')
+        self.log_file_abs_path = os.path.abspath(log_file_name)
+        handler = logging.FileHandler(self.log_file_abs_path, 'a')
         return handler
 
     def error(self, msg):
-        """Method that written in the log a message of type error
+        """Method that written in the log a text message of type error
 
         Keyword arguments:
         msg -- Message written in the log
@@ -44,7 +45,7 @@ class LoggerManager(object):
         self.logger.error(msg)
 
     def info(self, msg):
-        """Method that written in the log a message of type info
+        """Method that written in the log a text message of type info
 
         Keyword arguments:
         msg -- Message written in the log
@@ -55,13 +56,12 @@ class LoggerManager(object):
 
     def get_log(self):
         """Return the content of the log"""
-        text = ""
-        self.open_file = open(self.log_file_name, 'r')
-        text = self.open_file.read()
-        self.open_file.close()
+        open_file = open(self.log_file_abs_path, 'r')
+        text = open_file.read()
+        open_file.close()
         return str(text)
 
     def clear_log(self):
         """Clear the content of the log"""
-        self.open_file = open(self.log_file_name, 'w')
-        self.open_file.close()
+        open_file = open(self.log_file_abs_path, 'w')
+        open_file.close()
--- unit_test/test_logger.py
+++ unit_test/test_logger.py
@@ -11,37 +11,28 @@ __author__ = 'carlos^gonzales'
 class TestLogger(unittest.TestCase):
 
     def setUp(self):
-        player_name = "Player 1"
-        self.sudoku_game_log = LoggerManager(player_name)
+        log_id_name = "Player 1"
+        self.sudoku_log = LoggerManager(log_id_name)
 
-    def test_logging_a_set_cell(self):
+    def test_verify_that_is_possible_to_log_an_info_message(self):
         set_cell = SET + " A 1 6"
-        self.sudoku_game_log.info(set_cell)
-        text = self.sudoku_game_log.get_log()
+        self.sudoku_log.info(set_cell)
+        text = self.sudoku_log.get_log()
         self.assertTrue(set_cell in text)
 
-    def test_logging_a_hide_cell(self):
-        hint_cell = HIDE + " B 3"
-        self.sudoku_game_log.info(hint_cell)
-        text = self.sudoku_game_log.get_log()
-        self.assertTrue(hint_cell in text)
-
-    def test_logging_a_hint_cell(self):
-        hint_cell = HINT + " A 9"
-        self.sudoku_game_log.info(hint_cell)
-        text = self.sudoku_game_log.get_log()
-        self.assertTrue(hint_cell in text)
-
-    def test_logging_a_set_invalid_cell(self):
+    def test_verify_that_is_possible_to_log_an_error_message(self):
         invalid_cell = MESSAGE_INVALID_POSITION + " H 10 6"
-        self.sudoku_game_log.error(invalid_cell)
-        text = self.sudoku_game_log.get_log()
+        self.sudoku_log.error(invalid_cell)
+        text = self.sudoku_log.get_log()
         self.assertTrue(invalid_cell in text)
 
+    def test_verify_that_is_possible_to_clear_a_log(self):
+        self.sudoku_log.clear_log()
+        text = self.sudoku_log.get_log()
+        self.assertTrue(not text)
+
     def tearDown(self):
-        print "first " + self.sudoku_game_log.get_log()
-        self.sudoku_game_log.clear_log()
-        print "finally " + self.sudoku_game_log.get_log()
+        self.sudoku_log.clear_log()
 
 if __name__ == '__main__':
     unittest.main()
